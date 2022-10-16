fcb126382ff4ae5476b0485fe1cd0c6fcf43ef98
jordanmao <jordanmao2@gmail.com>
2022-10-16 01:14:49 -0400

    Added get_username function to TimsAppAPI to fix identification issues when submitting picks for other users

--- autopicker/tims_app_api.py
+++ autopicker/tims_app_api.py
@@ -18,9 +18,12 @@ logger = logging.getLogger(__name__)
 
 class TimsAppAPI:
     def __init__(self):
-        self.bearer_token = self.get_bearer_token()
+        bearer_token = self.__get_bearer_token()
+        self.access_token = bearer_token['AccessToken']
+        self.id_token = bearer_token['IdToken']
+        self.username = self.__get_username()
 
-    def get_bearer_token(self):
+    def __get_bearer_token(self):
         url = 'https://cognito-idp.us-east-1.amazonaws.com/'
         payload = json.dumps({
             "ClientId": CLIENT_ID,
@@ -42,19 +45,49 @@ class TimsAppAPI:
             response = requests.post(url, headers=headers, data=payload)
             response.raise_for_status()
         except HTTPError as http_err:
-            error_msg = 'HTTP error occured when trying to obtain bearer token'
+            error_msg = 'HTTP error occurred when trying to obtain bearer token'
             log_http_error(error_msg, logger, response, http_err)
         else:
             logger.debug('Obtained bearer token')
-            return response.json()['AuthenticationResult']['IdToken']
-        
+            return response.json()['AuthenticationResult']
+    
+    def __get_username(self):
+        url = "https://cognito-idp.us-east-1.amazonaws.com/"
+
+        payload = json.dumps({"AccessToken": self.access_token})
+        headers = {
+            'authority': 'cognito-idp.us-east-1.amazonaws.com',
+            'accept': '*/*',
+            'accept-language': 'en-CA,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6',
+            'cache-control': 'max-age=0',
+            'content-type': 'application/x-amz-json-1.1',
+            'origin': 'https://timhortons.ca',
+            'referer': 'https://timhortons.ca/',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
+            'user-agent': USER_AGENT,
+            'x-amz-target': 'AWSCognitoIdentityProviderService.GetUser',
+            'x-amz-user-agent': 'aws-amplify/5.0.4 js',
+            'x-requested-with': 'digital.rbi.timhortons'
+        }
+        try:
+            response = requests.post(url, headers=headers, data=payload)
+            response.raise_for_status()
+        except HTTPError as http_err:
+            error_msg = 'HTTP error occurred when trying to obtain username'
+            log_http_error(error_msg, logger, response, http_err)
+        else:
+            logger.debug('Obtained username')
+            return response.json()['Username']
+
     def get_games_and_players(self):
         url = 'https://px-api.rbi.digital/hockeyprod/picks'
         headers = {
             'authority': 'px-api.rbi.digital',
             'accept': 'application/json, text/plain, */*',
-            'x-cognito-id': 'us-east-1:09dc87b6-4be2-475e-ac4c-2e00cbc11bbf',
-            'authorization': f'Bearer {self.bearer_token}',
+            'x-cognito-id': f'us-east-1:{self.username}',
+            'authorization': f'Bearer {self.id_token}',
             'user-agent': USER_AGENT,
             'origin': 'https://timhortons.ca',
             'x-requested-with': 'digital.rbi.timhortons',
@@ -69,7 +102,7 @@ class TimsAppAPI:
             response = requests.get(url, headers=headers)
             response.raise_for_status()
         except HTTPError as http_err:
-            error_msg = 'HTTP error occured when trying to get games schedule and players sets from Tim Hortons app'
+            error_msg = 'HTTP error occurred when trying to get games schedule and players sets from Tim Hortons app'
             log_http_error(error_msg, logger, response, http_err)
         else:
             logger.info('Obtained game schedule and player sets from Tim Hortons app')
@@ -80,8 +113,8 @@ class TimsAppAPI:
         headers = {
             'authority': 'px-api.rbi.digital',
             'accept': 'application/json, text/plain, */*',
-            'x-cognito-id': 'us-east-1:09dc87b6-4be2-475e-ac4c-2e00cbc11bbf',
-            'authorization': f'Bearer {self.bearer_token}',
+            'x-cognito-id': f'us-east-1:{self.username}',
+            'authorization': f'Bearer {self.id_token}',
             'user-agent': USER_AGENT,
             'origin': 'https://timhortons.ca',
             'x-requested-with': 'digital.rbi.timhortons',
@@ -96,7 +129,7 @@ class TimsAppAPI:
             response = requests.get(url, headers=headers)
             response.raise_for_status()
         except HTTPError as http_err:
-            error_msg = 'HTTP error occured when trying to get pick history from Tim Hortons app'
+            error_msg = 'HTTP error occurred when trying to get pick history from Tim Hortons app'
             log_http_error(error_msg, logger, response, http_err)
         else:
             logger.info('Obtained pick history from Tim Hortons app')
@@ -124,8 +157,8 @@ class TimsAppAPI:
         headers = {
             'authority': 'px-api.rbi.digital',
             'accept': 'application/json, text/plain, */*',
-            'x-cognito-id': 'us-east-1:09dc87b6-4be2-475e-ac4c-2e00cbc11bbf',
-            'authorization': f'Bearer {self.bearer_token}',
+            'x-cognito-id': f'us-east-1:{self.username}',
+            'authorization': f'Bearer {self.id_token}',
             'user-agent': USER_AGENT,
             'content-type': 'application/json',
             'origin': 'https://timhortons.ca',
